439631ce0f7aae037d75572a90da6b3d5a4f4095
Stuart Todd <hello@halfshellstudios.co.uk>
2026-03-10 10:39:02 +0000

    Clear front end for new project

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Welcome');
 });
 
 Route::middleware([
